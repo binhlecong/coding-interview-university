fc7e5306c367006d1dd7b3e75767980bbe0fcc08
binhlecong <congbinh21012001@gmail.com>
2022-03-20 21:02:33 +0700

    add: bubble sort

---
+++ implementations/algorithms/sorting/bubble_sort.py
@@ -0,0 +1,26 @@
+from random import randint
+
+
+def test_sort(size, sort):
+    ans = []
+    while size > 0:
+        ans.append(randint(0, 200))
+        size -= 1
+    print('>>>', sort.__name__)
+    print('Before:\t', ans)
+    sort(ans)
+    print('After:\t', ans)
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    isSwapped = True
+    while isSwapped:
+        isSwapped = False
+        for i in range(1, n):
+            if arr[i - 1] > arr[i]:
+                arr[i - 1], arr[i] = arr[i], arr[i - 1]
+                isSwapped = True
+
+
+test_sort(10, bubble_sort)
