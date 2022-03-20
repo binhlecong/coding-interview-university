d84c19084c82222448346ad878a67e6345bdd2a6
binhlecong <congbinh21012001@gmail.com>
2022-03-20 19:26:35 +0700

    add: insertion sort

---
+++ implementations/algorithms/sorting/insertion_sort.py
@@ -0,0 +1,16 @@
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+arr = [12, 234, 34, 3, 23, 4, -23, 123]
+print(arr)
+
+insertion_sort(arr)
+print(arr)
