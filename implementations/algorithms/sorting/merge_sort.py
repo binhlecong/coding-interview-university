6480a6759b14b5a318fe21fc97394d177502db04
binhlecong <congbinh21012001@gmail.com>
2022-03-21 00:22:22 +0700

    add:merge sort and quick sort

---
+++ implementations/algorithms/sorting/merge_sort.py
@@ -0,0 +1,53 @@
+from random import randint
+
+
+def test_sort(size, sort):
+    ans = []
+    while size > 0:
+        ans.append(randint(0, 200))
+        size -= 1
+    print('>>>', sort.__name__)
+    print('Before:\t', ans)
+    sort(ans)
+    print('After:\t', ans)
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return
+    n = len(arr)
+    mid = n // 2
+
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    i = j = k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+
+# merge sort is a divide an conquere algorithm
+# not stable sort (Stable sort algorithms sort equal elements in the same order that they appear in the input)
+
+
+test_sort(10, merge_sort)
