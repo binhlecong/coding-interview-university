c4c65f24347b9ab63c1eebd153038edfbcfb22fb
binhlecong <congbinh21012001@gmail.com>
2022-05-03 08:21:13 +0700

    implementations\data_structures\tree\bst_sequences.py

---
+++ implementations/data_structures/tree/bst_sequences.py
@@ -0,0 +1,30 @@
+from itertools import permutations
+
+def bstToArrays(root):
+    arrs = [[root.data]]
+    s1 = [root]
+    s2 = []
+    while len(s1) != 0 or len(s2) != 0:
+        if len(s2) == 0:
+            while len(s1) != 0:
+                node = s1.pop()
+                if node.left:
+                    s2.append(node.left)
+                if node.right:
+                    s2.append(node.right)
+            perms = permutations(s2)
+        else:
+            while len(s2) != 0:
+                node = s2.pop()
+                if node.left:
+                    s1.append(node.left)
+                if node.right:
+                    s1.append(node.right)
+            perms = permutations(s1)
+
+        n = len(arrs)
+        for _ in range(n):
+            pre = arrs.pop(0)
+            for perm in perms:
+                arrs.append(pre + perm)
+
--- implementations/data_structures/tree/fcr_no_storing.py
+++ implementations/data_structures/tree/fcr_no_storing.py
@@ -2,14 +2,14 @@
 def findLCA(root, a, b):
     if root is None:
         return None
-    
+    # A child node is found
     if root.key == a or root.key == b:
         return root
     
     leftLca = findLCA(root.left, a, b)
     rightLca = findLCA(root.right, a, b)
-
+    # The common root is found
     if leftLca and rightLca:
         return root
-
+    #
     return leftLca if rightLca is None else rightLca
